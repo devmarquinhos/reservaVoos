2d0f757ea516f7dab75d7d664de4f2d52bbb5d2c
marquin.e <marcos.emanuel.04@gmail.com>
2023-01-19 17:07:49 -0300

    User agora possui nivel de influencia, 0 para usuário padrão e 1 para admin

--- app/models/tables.py
+++ app/models/tables.py
@@ -5,12 +5,14 @@ class User(db.Model): # Criando a tabela de users usando uma padrão do Flask
     
     # Criando a tabela
     id = db.Column(db.Integer, primary_key=True) # o 1º parâm. é o tipo de dado da coluna e o 2º é a primary key
+    systemInfluence = db.Column(db.Integer)
     nome = db.Column(db.String(120))
     senha = db.Column(db.String(60))
     email = db.Column(db.String(120), unique=True) # o 1º parâm. é o tipo de dado da coluna e o 2º é para dizer que é único por usuário
     
     # Marcando que todos os elementos passados como parâm. serão necessários para criação do usuário na tabela
-    def __init__(self, nome, senha, email):
+    def __init__(self,systemInfluence, nome, senha, email):
+        self.systemInfluence = systemInfluence
         self.nome = nome
         self.senha = senha
         self.email = email
