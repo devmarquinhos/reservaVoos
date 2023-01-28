ebf940b095e0a0a5f98f04af220070f2a8d73b3d
marquin.e <marcos.emanuel.04@gmail.com>
2023-01-28 20:28:07 -0300

    FLight form created at forms.py

--- app/models/forms.py
+++ app/models/forms.py
@@ -11,3 +11,12 @@ class RegisterForm(FlaskForm):
     username = fields.StringField("username", validators=[DataRequired()])
     password = fields.StringField("password", validators=[DataRequired()])
     confirm = fields.BooleanField("confirm", validators=[DataRequired()])
+    
+class RegisterFlight(FlaskForm):
+    flightNumber = fields.IntegerField("flightNumber", validators=[DataRequired()])
+    departureAirport = fields.StringField("departureAirport", validators=[DataRequired()])
+    arrivalAirport = fields.StringField("arrivalAirport", validators=[DataRequired()])
+    flightDate = fields.DateField("flightDate", validators=[DataRequired()])
+    aircraft = fields.StringField("aircraft", validators=[DataRequired()])
+    pax_available = fields.IntegerField("pax_available", validators=[DataRequired()])
+    pax_booked = fields.IntegerField("pax_booked", validators=[DataRequired()])
